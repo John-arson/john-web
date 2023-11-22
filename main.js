8c000fc3d7d1e28f330ee7cf467120dceb01ae6b
entyspite <151408521+shio-rin@users.noreply.github.com>
2023-11-21 17:06:03 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function searchfor() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("searcharticles");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("searchdropdown");
+  li = ul.getElementsByTagName("li");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (filter == "SEARCH=ALL") {
+      li[i].style.display = 'block';
+    } else {
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (filter.length < 1) {
+          li[i].style.display = 'none';
+        } else {
+          li[i].style.display = 'block';
+        }
+      } else {
+        li[i].style.display = 'none';
+      }
+    }
+  }
+}
+var liList = document.getElementById("searchdropdown").getElementsByTagName("li");
+var listcount = liList.length
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("toolbarheader").style.top = "0";
+    document.getElementById("search").style.top = "51px";
+  } else {
+    document.getElementById("toolbarheader").style.top = "-51px";
+    document.getElementById("search").style.top = "0";
+  }
+  prevScrollpos = currentScrollPos;
+} 
+function hide (elements) {
+  elements = elements.length ? elements : [elements];
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].style.display = 'none';
+  }
+}
+function show (elements) {
+  elements = elements.length ? elements : [elements];
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].style.display = 'block';
+  }
+}

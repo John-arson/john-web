cd75509fddc8d2096526b0adc9cc014f965c895f
entyspite <151408521+shio-rin@users.noreply.github.com>
2023-12-04 14:42:03 -0800

    Update main.js

--- files/js/main.js
+++ files/js/main.js
@@ -27,12 +27,26 @@ var listcount = liList.length
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
-  var hasTouchScreen = false;
+var hasTouchScreen = false;
 
-    if ("maxTouchPoints" in navigator) {
-      hasTouchScreen = navigator.maxTouchPoints > 0;
-  } 
-
+if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+} else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+} else {
+    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+    } else if ('orientation' in window) {
+        hasTouchScreen = true;
+    } else {
+        var UA = navigator.userAgent;
+        hasTouchScreen = (
+            /\b(BlackBerry|webOS|iPhone|Mobi|Safari|IEMobile)\b/i.test(UA) ||
+            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+        );
+    }
+}
   if (hasTouchScreen) {
     scrollnavbar();
   }

bfd6c842558dfb17dbb0dbbd891b31ac97e181c5
entyspite <151408521+shio-rin@users.noreply.github.com>
2023-12-05 07:52:45 -0800

    Create search.php

---
+++ files/js/templates/php/search.php
@@ -0,0 +1,15 @@
+<div class="search" id="search" style="display:none;">
+	<ul id="searchdropdown">
+		<li><a href="backups.htm">Backups</a></li>
+		<li><a href="banlist.htm">Ban list</a></li>
+		<li><a href="contributors.htm">Contributors</a></li>
+		<li><a href="directory.htm">Directory</a></li>
+		<li><a href="editorslist.htm">Editors list</a></li>
+		<li><a href="examples.htm">Examples</a></li>
+		<li><a href="index.htm">Home</a></li>
+		<li><a href="links.htm">Links</a></li>
+		<li><a href="lore.htm">Lore</a></li>
+		<li><a href="translations.htm">Translations</a></li>
+		<li><a href="variants.htm">Variants</a></li>
+	</ul>
+</div>
